d45f62b0fccd8bec9904a8232efaa40d4937a5d4
8Nomad8448 <v.tseiko8@gmail.com>
2021-06-16 14:21:13 +0300

    added sixth task

--- web/modules/custom/nomadmodule/src/Form/Nomadform.php
+++ web/modules/custom/nomadmodule/src/Form/Nomadform.php
@@ -7,6 +7,9 @@
 
 namespace Drupal\nomadmodule\Form;
 
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\HtmlCommand;
+use Drupal\Core\Ajax\InsertCommand;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 
@@ -32,10 +35,29 @@ class Nomadform extends FormBase {
       '#size' => 32,
       '#description' => t("The name of your pet, must contain at least 2 characters and maximum length is 32 characters, and can not contain any numbers, whitespaces, and symbols."),
       '#required' => TRUE,
+      '#ajax' => [
+        'callback' => '::validateNameAjax',
+        'event' => 'change',
+        'progress' => [
+          'type' => 'throbber',
+          'message' => t('Verifying name..'),
+        ],
+      ],
+    ];
+        $form['system_messages'] = [
+      '#markup' => '<div id="form-system-messages"></div>',
+      '#weight' => -100,
     ];
     $form ['submit'] = [
       '#type' => 'submit',
       '#value' => t('Add cat'),
+      '#ajax' => [
+        'callback' => '::ajaxSubmitCallback',
+        'event' => 'click',
+        'progress' => [
+          'type' => 'throbber',
+        ],
+      ],
     ];
     return $form;
   }
@@ -49,6 +71,25 @@ class Nomadform extends FormBase {
     if (!preg_match('/^[A-Za-z]*$/', $value) || strlen($value)<2 || strlen($value)>32) {
       $form_state->setErrorByName ('name', t('The name %name is not valid.', array('%name' => $value)));
     }
+  }
+  
+    /**
+   * {@inheritdoc}
+   */
+  public function ajaxSubmitCallback(array &$form, FormStateInterface $form_state) {
+    $ajax_response = new AjaxResponse();
+    $message = [
+      '#theme' => 'status_messages',
+      '#message_list' => $this->messenger()->all(),
+      '#status_headings' => [
+        'status' => t('Status message'),
+        'error' => t('Error message'),
+        'warning' => t('Warning message'),
+      ],
+    ];
+    $messages = \Drupal::service('renderer')->render($message);
+    $ajax_response->addCommand(new HtmlCommand('#form-system-messages', $messages));
+    return $ajax_response;
   }
   
   /**
